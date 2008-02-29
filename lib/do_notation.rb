b9988bc5e59cd77fd782fb21b8e2b859b8b80008
Aanand Prasad <aanand@aanand.local>
2008-02-29 17:32:41 +0000

    Moved DoNotation into separate file

---
+++ lib/do_notation.rb
@@ -0,0 +1,75 @@
+require 'parse_tree'
+require 'sexp_processor'
+
+class DoNotation < SexpProcessor
+  def process_bmethod exp
+    type = exp.shift
+    
+    if arg_assignment = process(exp.shift)
+      if arg_assignment.first == :dasgn or arg_assignment.first == :dasgn_curr
+        args = [arg_assignment[1]]
+      elsif arg_assignment.first == :masgn
+        args = arg_assignment[1][1..-1].collect { |e| e[1] }
+      else
+        raise DoNotationError, "I can't parse this block :("
+      end
+    else
+      args = []
+    end
+    
+    block = process(exp.shift)
+    
+    assert_type block, :block
+    block.shift
+    
+    s(:scope,
+      s(:block,
+        s(:args, *args),
+        *rewrite_assignments(block)))
+  end
+  
+  def rewrite_assignments exp
+    return [] if exp.empty?
+    
+    head = exp.shift
+    
+    if head.first == :call and head[1].first == :vcall and head[2] == :< and head[3].first == :array and head[3][1].last == :-@
+      var_name = head[1][1]
+      expression = head[3][1][1]
+      
+      body = rewrite_assignments(exp)
+      
+      if body.first.is_a? Symbol
+        body = [s(*body)]
+      end
+      
+      [s(:iter,
+         s(:call, process(expression), :bind),
+         s(:dasgn_curr, var_name),
+         *body)]
+    else
+      head + rewrite_assignments(exp)
+    end
+  end
+  
+  def self.pp(obj, indent='')
+    return obj.inspect unless obj.is_a? Array
+    return '()' if obj.empty?
+
+    str = '(' + pp(obj.first, indent + ' ')
+
+    if obj.length > 1
+      str << ' '
+
+      next_indent = indent + (' ' * str.length)
+
+      str << obj[1..-1].map{ |o| pp(o, next_indent) }.join("\n#{next_indent}")
+    end
+
+    str << ')'
+
+    str
+  end
+end
+
+class DoNotationError < StandardError; end
