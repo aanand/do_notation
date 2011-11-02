02a052c70488b552cf92974d3e6c88dd82c5ef84
Aanand Prasad <aanand.prasad@gmail.com>
2011-11-02 12:52:42 +0000

    Specify that blocks can be run multiple times by different monads
    Currently fails because Rewriter is destructive (as are SexpProcessors
    in general), and Proc#to_sexp appears to cache its return value.

--- spec/monad_spec.rb
+++ spec/monad_spec.rb
@@ -1,5 +1,7 @@
 require File.join(File.dirname(__FILE__), 'spec_helper')
+
 require 'do_notation/monads/array'
+require 'do_notation/monads/maybe'
 
 describe "Monad.run" do
   specify "should have lexical scope" do
@@ -18,7 +20,18 @@ describe "Monad.run" do
                      "second cousin once removed",
                      "second cousin twice removed"]
   end
-  
+
+  it "can be run on the same block multiple times, using different monads" do
+    block = proc do
+      x <- unit(1)
+      y <- unit(2)
+      unit(x+y)
+    end
+
+    Array.run(&block).should == [3]
+    Maybe.run(&block).should == Maybe.new(3)
+  end
+
   specify "should be nestable" do
     array = Array.run do
       x <- run do
